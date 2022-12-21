f4c32f2f42c7b6527c3246fb300438519d48d246
Thinh Nguyen <thinhnd194@gmail.com>
2022-12-22 00:36:13 +0700

    init project

--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"go-crud/initializers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+}
 
 func main() {
 	r := gin.Default()
